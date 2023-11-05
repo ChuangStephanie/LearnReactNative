0993905ee4c31425a0e323eaa998f0bc8d73cd09
ChuangStephanie <stephanie231951@yahoo.com>
2023-11-05 04:50:45 -0600

    completed tabs

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -40,10 +40,17 @@ const JobDetails = () => {
         case "Qualifications":
             return <Specifics 
                 title="Qualifications"
-                points={data[0].job_highlights?.qualifications ?? "N/A"}
+                points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
             />
         case "About":
+            return <JobAbout 
+                info={data[0].job_description ?? "No data provided"}
+            />
         case "Responsibilities":
+            return <Specifics 
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+        />
         default:
             break;
     }
@@ -60,7 +67,7 @@ const JobDetails = () => {
             <ScreenHeaderBtn
               iconUrl={icons.left}
               dimension="60%"
-              handlePress={() => router.back}
+              handlePress={() => router.back()}
             />
           ),
           headerRight: () => (
