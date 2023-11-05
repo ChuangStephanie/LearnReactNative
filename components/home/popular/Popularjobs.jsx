36f2fffa0b2e31262b93b14db6381cb575426b00
ChuangStephanie <stephanie231951@yahoo.com>
2023-11-05 01:48:24 -0500

    created useFetch hook

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,11 +11,16 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "react Developer",
+    num_pages: 1,
+  });
+
+  //console.log(data);
 
   return (
     <View style={styles.container}>
@@ -25,23 +30,21 @@ const Popularjobs = () => {
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
-      
+
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary}/>
+          <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong</Text>
-        ) : <FlatList
-          data = {[1, 2, 3, 4, 5, 6, 7, 8]}
-          renderItem={({item}) => (
-            <PopularJobCard
-              item = {item}
-            />
-          )}
-          keyExtractor={item => item?.job_id}
-          contentContainerStyle={{columnGap: SIZES.medium}}
-          horizontal
-        />}
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
       </View>
     </View>
   );
